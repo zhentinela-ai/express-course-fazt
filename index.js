2ac356bbaebb53613ce4d79055fd1c2036375f0a
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-09-22 21:28:34 -0500

    second session

--- index.js
+++ index.js
@@ -14,10 +14,29 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile("./static/index.html", {
-    root: __dirname,
-  });
+  res.send('Hello world')
+  // res.sendFile("./static/index.html", { root: __dirname });
 });
+
+app.get("/products", (req, res) => {
+  res.send("Lista de productos")
+})
+
+app.post("/products", (req, res) => {
+  res.send("Crear producto")
+})
+
+app.put("/products", (req, res) => {
+  res.send("Actualizando producto")
+})
+
+app.delete("/products", (req, res) => {
+  res.send("Eliminando producto")
+})
+
+app.patch("/products", (req, res) => {
+  res.send("Actualizando una parte del producto")
+})
 
 app.listen(3000);
 console.log(`Server on port ${3000}`);
