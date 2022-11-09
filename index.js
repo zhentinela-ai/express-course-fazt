6102e1a7fc9d83ae7067978213b9db3a4deec70e
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-11-09 01:26:12 -0500

    third session

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const express = require("express");
+
+const app = express();
+
 // const http = require("http");
 // const fs = require("fs");
 
@@ -9,34 +13,100 @@
 // server.listen(3000);
 // console.log(`Server on port ${3000}`);
 
-const express = require("express");
+// app.get("/", (req, res) => {
+//   res.send("Hello world");
+//   // res.sendFile("./static/index.html", { root: __dirname });
+// });
 
-const app = express();
+// app.get("/products", (req, res) => {
+//   res.send("Lista de productos");
+// });
 
-app.get("/", (req, res) => {
-  res.send('Hello world')
-  // res.sendFile("./static/index.html", { root: __dirname });
+// app.post("/products", (req, res) => {
+//   res.send("Crear producto");
+// });
+
+// app.put("/products", (req, res) => {
+//   res.send("Actualizando producto");
+// });
+
+// app.delete("/products", (req, res) => {
+//   res.send("Eliminando producto");
+// });
+
+// app.patch("/products", (req, res) => {
+//   res.send("Actualizando una parte del producto");
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello world");
+// });
+
+// app.get("/miarchivo", (req, res) => {
+//   res.sendFile("./elegante.png", {
+//     root:  __dirname,
+//   });
+// });
+
+// app.get("/user", (req, res) => {
+//   res.json({
+//     name: "zhent",
+//     lastname: "ray",
+//     age: 25,
+//     points: [1, 2, 3, 4, 5],
+//     address: {
+//       street: "Calle falsa",
+//       number: 123,
+//     },
+//     hello: function () {
+//       return "Hello world";
+//     },
+//   });
+// });
+
+// app.get("/isAlive", (req, res) => {
+//   res.sendStatus(204)
+// });
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+// app.post("/user", (req, res) => {
+//   console.log("Body", req.body);
+//   res.send("Creando usuario");
+// });
+
+app.all("/info", (req, res) => {
+  res.send(req.headers);
+})
+
+app.get("/search", (req, res) => {
+  console.log("Query", req.query);
+  if (req.query.q === "javascript books") {
+    res.send("lista de libros javascript");
+  } else {
+    res.send("pagina normal");
+  }
 });
 
-app.get("/products", (req, res) => {
-  res.send("Lista de productos")
-})
+app.get("/hello/:username", (req, res) => {
+  res.send("Hello " + req.params.username.toUpperCase());
+});
 
-app.post("/products", (req, res) => {
-  res.send("Crear producto")
-})
+app.get("/add/:x/:y", (req, res) => {
+  const { x, y } = req.params;
+  res.send("La suma es " + (parseInt(x) + parseInt(y)));
+});
 
-app.put("/products", (req, res) => {
-  res.send("Actualizando producto")
-})
+app.get("/users/:username/photo", (req, res) => {
+  if (req.params.username === "zhent") {
+    return res.sendFile("./elegante.png", {
+      root: __dirname,
+    });
+  }
 
-app.delete("/products", (req, res) => {
-  res.send("Eliminando producto")
-})
-
-app.patch("/products", (req, res) => {
-  res.send("Actualizando una parte del producto")
-})
+  res.send("No existe el usuario");
+});
 
 app.listen(3000);
 console.log(`Server on port ${3000}`);
